87a3a52de7bbd0c4e0ffaacc4b8590c9a82a88b9
Stefan Finke <finke@poprocket.com>
2023-03-08 13:10:48 +0100

    init setup

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React, { ReactElement } from 'react';
+import StartPage from './pages/start-page';
+
+export default function App(): ReactElement {
+  return <StartPage />;
+}
---
+++ src/pages/start-page.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+export default function StartPage(): ReactElement {
+  return (
+    <div className="bg-black w-full h-full flex justify-center items-center">
+      <h1 className="text-xl text-white">Randomizer Note Clicker</h1>
+    </div>
+  );
+}
